9afc68f4cf8b1df6636d40ac52059c7b6cf9ddee
SalmasKit <salma.barrak.ensao@ump.ac.ma>
2025-11-04 20:06:17 +0100

    FIX: Project Structure + Start emplementing settings

---
+++ app/src/main/java/com/example/soukify/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.soukify;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.soukify.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Make the status bar transparent
+        getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Set up navigation
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+}
