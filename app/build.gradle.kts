23703c625e5896192c8ff6edbe710b14696fb951
Oulkabous Safae <oulkaboussafae@gmail.com>
2025-11-22 10:53:04 +0100

    dernier modification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,5 +58,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.cardview:cardview:1.0.0")
 }
 
